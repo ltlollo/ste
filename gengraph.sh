5332d25432a37f8785b137908f1f0e330a5986b6
ltlollo <ltlollo.it@gmail.com>
2017-05-19 16:18:42 +0200

    format

--- gengraph.sh
+++ gengraph.sh
@@ -1,4 +1,4 @@
-funcs() { cscope -kL0 ".*" $1 | awk '{ print $2 }' | grep -v "<global>"| sort -u; }
-calls() { while read i; do cscope -L3 $i| awk -v c="$i" '{ printf "%s->%s\n",$2,c }'; done; }
-graph() { echo "digraph iftree {"; funcs "$1" | calls | sort -u; echo "}"; } > "dot$1"
+funcs() { cscope -kL0 ".*" $@ | awk '{ print $2 }' | grep -v "<global>"| sort -u; }
+calls() { while read i; do cscope -kL3 $i | awk -vc="$i" '{ printf "%s->%s\n",$2,c }'; done; }
+graph() { echo "digraph iftree {"; funcs $@ | calls | sort -u; echo "}"; } > "dot$1"
 graph "$1" && dot -Grankdir=LR -Gordering=out -Tpng "dot$1" -o "g$1.png" && feh "g$1.png"
